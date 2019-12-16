2e94d3770355a2dba4fd2715bf3894da02d27671
Michael Davis <michael@damaru.com>
2019-12-16 14:45:00 -0500

    array support

--- src/main/java/com/solace/tester/SolaceGeneratorTesterApplication.java
+++ src/main/java/com/solace/tester/SolaceGeneratorTesterApplication.java
@@ -1,7 +1,5 @@
 package com.solace.tester;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 import com.solace.asyncapi.Account;
 import com.solace.asyncapi.Address;
+import com.solace.asyncapi.Item;
 import com.solace.asyncapi.Order;
 import com.solace.asyncapi.Order.Shipping;
 import com.solace.asyncapi.OrderChannel;
@@ -21,10 +20,10 @@ import com.solace.asyncapi.OrderQueueChannel;
 
 @SpringBootApplication
 @ComponentScan("com.solace")
-public class SolaceBeanTesterApplication implements CommandLineRunner {
+public class SolaceGeneratorTesterApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SolaceBeanTesterApplication.class, args);
+		SpringApplication.run(SolaceGeneratorTesterApplication.class, args);
 	}
 
 	@Autowired
@@ -40,36 +39,55 @@ public class SolaceBeanTesterApplication implements CommandLineRunner {
 		Runtime.getRuntime().addShutdownHook(new ShutdownHook(orderChannel, orderQueueChannel));
 		testSubscribe();
 		testPublish();
-		Thread.sleep(5000);
+		Thread.sleep(1000);
 	}
 
 	public void testSubscribe() throws Exception {
 		orderChannel.subscribe(new OrderSubscribeListener());
-		//orderQueueChannel.subscribe(new OrderQueueSubscribeListener());
+		orderQueueChannel.subscribe(new OrderQueueSubscribeListener());
 	}
 
 	public void testPublish() throws Exception {
 		orderChannel.initPublisher(new OrderPublishListener());
 		
 		Address address = new Address();
-		address.setStreetAddress("123 Fake St");
-		address.setCity("Carp");
-		address.setPostalCode("K0A 1L0");
-		address.setProvince("ONT");
+		address
+		.setStreetAddress("123 Fake St")
+		.setCity("Carp")
+		.setPostalCode("K0A 1L0")
+		.setProvince("ONT");
 		
 		Account account = new Account();
-		account.setAccountId(12345);
-		account.setFirstName("Angela");
-		account.setLastName("Aarons");
+		account
+		.setAccountId(12345)
+		.setFirstName("Angela")
+		.setLastName("Aarons");
 		
 		Shipping shipping = new Shipping();
-		shipping.setCost(22.50);
-		shipping.setMethod(Shipping.Method.courier);
-		shipping.setShipTo(address);
+		shipping
+		.setCost(22.50)
+		.setMethod(Shipping.Method.courier)
+		.setShipTo(address);
 		
 		Order order = new Order();
-		order.setCustomer(account);
-		order.setShipping(shipping);
+		order
+		.setCustomer(account)
+		.setShipping(shipping);
+		
+		Item[] items = new Item[2];
+		Item i1 = new Item();
+		i1.setCatalogId(123);
+		i1.setDescription("Dish rack");
+		i1.setPrice(20.00);
+		items[0] = i1;
+		
+		Item i2 = new Item();
+		i2.setCatalogId(123);
+		i2.setDescription("Ferrari");
+		i2.setPrice(200000.00);
+		items[1] = i2;
+		
+		order.setItems(items);
 		
 		OrderMessage orderMessage = new OrderMessage();
 		orderMessage.setPayload(order);
@@ -77,10 +95,8 @@ public class SolaceBeanTesterApplication implements CommandLineRunner {
 		int span = 1;
 		for (int i = 0; i < 10; i++) {
 			order.setOrderId(i);
-			int price = 200 + i * 320;
 			order.setOrderDescription("I'm order # " + i);
-			BigDecimal bd = new BigDecimal(2 + i * 3.2);
-			bd.setScale(2, RoundingMode.FLOOR);
+			int price = 200 + i * 320;
 			order.setPrice(price / 100.0);
 			orderChannel.sendOrderMessage(orderMessage, OrderChannel.Action.buyItem, "trace", span++);
 			orderChannel.sendOrderMessage(orderMessage, OrderChannel.Action.returnItem, "trace", span++);
